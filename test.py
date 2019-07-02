46aab16a671e482e12f956797abe372d4f913f86
abinashdash17 <abinashdash17@gmail.com>
2019-07-02 20:58:35 +0530

    completed ex21.py

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-def gennum() :
-    i = 0
-    while True :
-        yield i
-        i += 1
-x = gennum()
-y = gennum()
-for i in x :
-    if i%3 == 0:
-        next(y)
-    print(x,y)
-    if i == 23 :
-        break
-
+from math import sqrt
+print(sqrt(9))
