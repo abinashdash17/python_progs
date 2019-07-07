679570e28a84eb973cd755e45263786590ef68b7
Abinash Dash <abinashdash17@gmail.com>
2019-07-07 23:29:18 +0530

    added ex48

---
+++ ex47.py
@@ -0,0 +1,3 @@
+x = [ i for i in range(1,21) ]
+even = list(filter(lambda i : i%2 == 0, x))
+print(even)
---
+++ ex48.py
@@ -0,0 +1,2 @@
+ev_sq = map(lambda i : i ** 2 , filter(lambda i : i%2 == 0, range(1,21)))
+print(list(ev_sq))
