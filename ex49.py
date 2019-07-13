530cfd1b13a85940122c07a9b325da9108d1fb90
Abinash Dash <abinashdash17@gmail.com>
2019-07-14 00:43:15 +0530

    ex49 from pc added

---
+++ ex49.py
@@ -0,0 +1,8 @@
+class Indian():
+    @staticmethod #printNationality=staticmethod(printNationality)
+    def printNationality():
+        print("India")
+
+anIndian = Indian()
+anIndian.printNationality()
+Indian.printNationality()
